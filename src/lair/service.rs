877817020e2e4e8b8ab0486cf18a135012f8e9e6
inkyu <gofiri@gmail.com>
2025-04-01 14:40:57 +0900

    Use sark

--- src/lair/service.rs
+++ src/lair/service.rs
@@ -1,76 +1,88 @@
 use std::cell::RefCell;
-use std::convert::Infallible;
-use std::future::Future;
-use std::pin::Pin;
 use std::rc::Rc;
+use sark::prelude::*;
+use bytes::BytesMut;
+use http;
 
-use http_body_util::combinators::UnsyncBoxBody;
-use http_body_util::{BodyExt, Full};
-use hyper::body::{Bytes, Incoming};
-use hyper::{Request, Response};
-use tower_http::services::{ServeDir, ServeFile};
-use tower_service::Service;
 
-#[derive(Debug, Clone)]
-pub struct LairService {
+pub struct LairHandler {
     counter: Rc<RefCell<i32>>,
-    assets: ServeDir,
-    index: ServeFile,
 }
 
-impl LairService {
-    pub fn new() -> Self {
-        Self {
-            counter: Rc::new(RefCell::new(0)),
-            assets: ServeDir::new(""),
-            index: ServeFile::new("resources/index.html"),
+impl Service<()> for LairHandler {
+    async fn call(&self, req: Request, _: &()) -> Result<Response> {
+        tracing::info!("{} {}", req.method(), req.uri().path());
+        
+        let result = match (req.method(), req.uri().path()) {
+            (&http::Method::GET, "/") => self.index(req).await,
+            (&http::Method::GET, "/count") => self.count(req).await,
+            (_, path) if path.starts_with("/assets/") => self.assets(req).await,
+            _ => Err(Error::NotFound),
+        };
+        
+        if let Err(ref e) = result {
+            tracing::error!("Error: {}", e);
+        }
+        
+        result
+    }
+}
+
+impl LairHandler {
+    async fn index(&self, _req: Request) -> Result<Response> {
+        tracing::debug!("Serving index page");
+        let html_content = match std::fs::read_to_string("resources/index.html") {
+            Ok(content) => content,
+            Err(_) => return Err(Error::NotFound)
+        };
+
+        let mut resp = Response::ok();
+        resp.headers_mut().insert(
+            http::header::CONTENT_TYPE,
+            http::HeaderValue::from_static("text/html")
+        );
+        resp.set_body_str(&html_content);
+        Ok(resp)
+    }
+
+    async fn count(&self, req: Request) -> Result<Response> {
+        tracing::debug!("Serving count page");
+        if req.uri().path() != "/favicon.ico" {
+            *self.counter.borrow_mut() += 1;
+        }
+        
+        let mut resp = Response::ok();
+        resp.set_body_str(&format!("Hello from Lair! Visit count: {}", self.counter.borrow()));
+        Ok(resp)
+    }
+
+    async fn assets(&self, req: Request) -> Result<Response> {
+        let path = req.uri().path();
+        let asset_path = path.strip_prefix("/assets/").unwrap_or("");
+        tracing::debug!("Serving asset: {}", asset_path);
+
+        if asset_path == "fjalla-one.woff2" || asset_path == "nanum.woff2" {
+            let file_path = format!("assets/{}", asset_path);
+            let font_bytes = std::fs::read(file_path).map_err(|_| Error::NotFound)?;
+
+            let mut resp = Response::ok();
+            resp.headers_mut().insert(
+                http::header::CONTENT_TYPE,
+                http::HeaderValue::from_static("font/woff2")
+            );
+            resp.set_body(BytesMut::from(&font_bytes[..]));
+            Ok(resp)
+        } else {
+            Err(Error::NotFound)
         }
     }
 }
 
-impl hyper::service::Service<Request<Incoming>> for LairService {
-    type Response = Response<UnsyncBoxBody<Bytes, std::io::Error>>;
-    type Error = Infallible;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+pub struct LairService;
 
-    fn call(&self, req: Request<Incoming>) -> Self::Future {
-        fn mk_response(
-            s: String,
-        ) -> Result<Response<UnsyncBoxBody<Bytes, std::io::Error>>, Infallible> {
-            Ok(Response::builder()
-                .body(UnsyncBoxBody::new(Full::new(Bytes::from(s)).map_err(
-                    |_| std::io::Error::new(std::io::ErrorKind::Other, "oh no!"),
-                )))
-                .unwrap())
-        }
-
-        if req.uri().path() != "/favicon.ico" {
-            *self.counter.borrow_mut() += 1;
-        }
-
-        if req.uri().path().starts_with("/assets/") {
-            let mut assets = self.assets.clone();
-            return Box::pin(async move {
-                let res = assets.call(req).await;
-                let res = res.map(|resp| resp.map(|body| body.boxed_unsync()));
-                println!("{:?}", res);
-                res
-            });
-        }
-
-        let res = match req.uri().path() {
-            "/" => {
-                let mut index = self.index.clone();
-                return Box::pin(async move {
-                    let res = index.call(req).await;
-                    let res = res.map(|resp| resp.map(|body| body.boxed_unsync()));
-                    println!("{:?}", res);
-                    res
-                });
-            }
-            _ => mk_response("not found".into()),
-        };
-
-        Box::pin(async { res })
+impl LairService {
+    pub fn new() -> impl Service<()> {
+        let counter = Rc::new(RefCell::new(0));
+        LairHandler { counter }
     }
 }
--- src/server.rs
+++
@@ -1,4 +0,0 @@
-mod body;
-mod tokio_io;
-
-pub use tokio_io::TokioIo;
